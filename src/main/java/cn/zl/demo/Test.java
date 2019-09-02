0a1170ccbf398c8532bd85ede4052beb523e4e6a
ray_glo <ray_glo@zhoulei.com>
2019-09-02 10:28:25 +0800

    first commit

---
+++ src/main/java/cn/zl/demo/Test.java
@@ -0,0 +1,11 @@
+package cn.zl.demo;
+
+/**
+ * @author ray2hou
+ * @create 2019-09-02-9:53
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
